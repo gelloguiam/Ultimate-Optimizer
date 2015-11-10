900a71550bf4961828f616c110ea30cd3706d8a7
gelloguiam <acguiam@gmail.com>
2015-11-10 18:05:49 +0800

    UI Structure

--- js/controller/MainController.js
+++ js/controller/MainController.js
@@ -1,8 +1,25 @@
 var ulopt = angular.module('ultimateOptimizer', []);
 
 ulopt.controller('MainController', ['$scope', function($scope){
-	$scope.data = {};
-	$scope.save = function(data){
-		$scope.data = angular.copy(data);
+	$scope.tableau = new Object();
+	$scope.objFxn = new Array();
+	$scope.goal;
+
+	$scope.data = new Object();
+	$scope.csv = "";
+
+	$scope.minimize = function() {
+		$scope.goal = true; //set if minimize button is clicked
+		console.log($scope.goal);
 	}
+
+	$scope.maximize = function() {
+		$scope.goal = false; //set if maximize button is clicked
+		console.log($scope.goal);
+	}
+
+	$scope.convertToCSV = function() {
+		var length = Object.keys($scope.data).length; //get length of object data
+	}
+
 }]);
