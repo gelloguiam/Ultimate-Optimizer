f6124f768b8913b4c0e7f0227e75ebe02cfea381
gelloguiam <acguiam@gmail.com>
2015-11-13 00:18:41 +0800

    json data

--- js/controller/MainController.js
+++ js/controller/MainController.js
@@ -1,25 +1,17 @@
 var ulopt = angular.module('ultimateOptimizer', []);
 
-ulopt.controller('MainController', ['$scope', function($scope){
-	$scope.tableau = new Object();
-	$scope.objFxn = new Array();
-	$scope.goal;
+ulopt.controller('MainController', function($scope){
+	$scope.contents = null;
 
-	$scope.data = new Object();
-	$scope.csv = "";
-
-	$scope.minimize = function() {
-		$scope.goal = true; //set if minimize button is clicked
-		console.log($scope.goal);
+	$scope.loadData = function() {
+		$.getJSON("json/data.json", function(data){
+			$scope.contents = data;
+			console.log(data);
+			alert("HEHE");
+		});
 	}
 
-	$scope.maximize = function() {
-		$scope.goal = false; //set if maximize button is clicked
-		console.log($scope.goal);
-	}
 
-	$scope.convertToCSV = function() {
-		var length = Object.keys($scope.data).length; //get length of object data
-	}
+});
 
-}]);
+
