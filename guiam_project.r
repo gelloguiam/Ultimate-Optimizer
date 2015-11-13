25957d3221e541a39b7da21ef1a69cd9096816ee
gelloguiam <acguiam@gmail.com>
2015-11-14 00:37:20 +0800

    simplex import/export data

--- guiam_project.r
+++ guiam_project.r
@@ -19,13 +19,24 @@ GaussJordan = function(mat, rowCount, colCount, pivotElementIndex, pivotColumnIn
   return(mat); #return matrix result of Gauss Jordan
 }
 
-UltimateOptimizer = function(mat) {
+UltimateOptimizer = function() {
+  mat = read.csv("data.csv"); #read data from the directory
+  #reference: http://www.r-tutor.com/r-introduction/data-frame/data-import
+  
+  currentDirectory = getwd(); #get working directory
+  folder = "iterations";
+  
+  if(!file.exists(folder)) #check if subdirectory iterations exist
+  dir.create(file.path(currentDirectory, folder)); #create sub directory if not
+  #reference: http://stackoverflow.com/questions/4216753/check-existence-of-directory-and-create-if-doesnt-exist
+  
   rowCount = nrow(mat); #get row count
   colCount = ncol(mat); #get column count
-  hehe = 0;
+  iteration = 1;
   
   while(TRUE) {
     minval = min(mat[rowCount,]); #get minimum value of the last row
+    
     if(minval >= 0) break;
 
     pivotColumnIndex = which(mat[rowCount,] == minval); #get index of the min value
@@ -42,7 +53,14 @@ UltimateOptimizer = function(mat) {
     mat[pivotElementIndex,] = mat[pivotElementIndex,]/pivotElement; #normalize row
     
     mat = GaussJordan(mat, rowCount, colCount, pivotElementIndex, pivotColumnIndex); #perform Gauss Jordan
+    
+    fileName = paste("iterations/iteration_",iteration,".csv", sep="");
+    print(fileName);
+    write.table(mat, file=fileName, row.names=FALSE, col.names=FALSE, sep=",");
+    #reference: http://rprogramming.net/write-csv-in-r/
+
     print(mat);
+    iteration = iteration + 1;
   }
 
 }
